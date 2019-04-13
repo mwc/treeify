80552c0e79c460353c7c96860fa4179489c2cf4d
MaoWenChao <mwc@foxmail.com>
2019-04-14 00:29:47 +0800

    add `untreeify` function, `treeify` use configuration.

--- src/treeify.js
+++ src/treeify.js
@@ -1,64 +1,135 @@
 /*!
- * treeify.js v0.0.1 - 2018/4
- * 
- * 数组转换为树形结构对象
+ * treeify.js v1.0.2 - 2019/4
+ * Convert an array to a tree object.
  * 
  * Copyright (C) mwc@foxmail.com
  * 2018-present.
  */
 
-/**
- * 数组转换为树形结构对象
- * 
- * 作为数据源的数组，其元素必须为对象，对象保存着当前唯一的id（或其他唯一标识符），
- * 以及其父id；
- * 
- * 这两项属性是 treeify() 必须的。
- * 
- * 若非默认属性名，可通过 idKey、parentIdKey 提供指定的名称；
- * 
- * childrenName 参数提供的名称字符串，用于给元素创建其子元素数组的属性名，
- * 若未设置则默认为 'children'，如：
- * 
- * { id: 'a1', parentId: 'a', children: [] }
- * 
- * 可通过自定义子元素集合的属性名称，如使用 'child'：
- * 
- * treeify([...], 'id', 'parentId', 'child');
- * 
- * // 结果：  { id: 'a1', parentId: 'a', child: [] }
- * 
- * @param {Array} data 数据的一维数组
- * @param {string} idKey 代表id的键名，默认为 id
- * @param {string} parentIdKey 代表父id的键名，默认为 parentId
- * @param {string} childrenName 存放子对象的属性名，默认为 children
- */
-export default function treeify(data, idKey = 'id', parentIdKey = 'parentId', childrenName = 'children') {
-    const disposable = {}; // 临时对象
-    let root; // 根节点
-    let parentId; // 父id
-    let id; // 当前元素id
-
-    for (let item of data) {
-        id = item[idKey];
-        parentId = item[parentIdKey];
-
-        if (!(id in disposable)) {
-            disposable[id] = [];
-        }
-
-        if (!(parentId in disposable)) {
-            disposable[parentId] = [];
-        }
-
-        item[childrenName] = disposable[id];
-
-        if (parentId) {
-            disposable[parentId].push(item);
-        } else {
-            root = item;
-        }
-    }
-
-    return root;
+function executeIfFunction(f) {
+	return typeof f === "function" ? f(this) : f
 }
+
+function deepClone(o) {
+	return Object.keys(o).reduce((target, key) => {
+		let value = o[key]
+		target[key] = value instanceof Object ? deepClone(value) : value
+		return target
+	}, Array.isArray(o) ? [] : {})
+}
+
+/**
+ * Convert an array to a tree object.
+ * 
+ * Note: An array which as data source, all element of it must be an object that has an unique ID and a parent's ID.
+ * 
+ * @param {Array}   data        Give a data source to treeify
+ * @param {Object}  configure   Configure keys name mapping in convertion
+ */
+function treeify(data, configure) {
+	const config = Object.assign(
+		{
+			id: "id",
+			parentId: "parentId",
+			children: "children",
+			root: null,
+			multi: false,           // If the 'multi' is set to TRUE, it can have multiple roots, FALSE can only have one.
+			deepClone: false        // Whether deep clone all elements of data in convert.
+		},
+		configure
+	)
+
+	const disposable = {}   // an object which is temp used.
+
+	let roots = []          // Root node
+	const setRoots = (el) => {
+		if (config.multi) {
+			roots.push(el)
+		} else {
+			roots = el
+		}
+	}
+
+	let id                  // ID of current element
+	let parentId            // parent ID of current element
+
+	for (let el of data) {
+		if (config.deepClone) {
+			el = deepClone(el)
+		}
+
+		const f = executeIfFunction.bind(el)
+
+		id = el[f(config.id)]
+		parentId = el[f(config.parentId)]
+
+		if (!(id in disposable)) {
+			disposable[id] = []
+		}
+
+		if (!(parentId in disposable)) {
+			disposable[parentId] = []
+		}
+
+		el[f(config.children)] = disposable[id]
+
+		const root = f(config.root)
+
+		do {
+			if (config.root || !parentId) {
+				if (config.root) {
+					if ((typeof root == "boolean" && root) ||
+						(Array.isArray(root) && root.includes(parentId)) ||
+						(parentId === root)
+					) {
+						setRoots(el)
+						break
+					}
+				} else if (!parentId) {
+					setRoots(el)
+					break
+				}
+			}
+
+			disposable[parentId].push(el)
+		} while (false)
+	}
+
+	return roots
+}
+
+/**
+ * Convert a tree from treeify() back into an array.
+ * This is the inverse of treeify.
+ * 
+ * @param {Object} tree             A tree comes from treeify
+ * @param {String} childrenName     Give the children's key name, it can also be a function.
+ */
+function untreeify(tree, childrenName = "children") {
+	let result = []
+
+	if (!childrenName) {
+		throw new Error("The parameter 'childrenName' must be specified.")
+	}
+
+	if (Array.isArray(tree)) {
+		result = tree
+	} else {
+		result = [tree]
+	}
+
+	result.forEach(item => {
+		const childKey = executeIfFunction.bind(item)(childrenName)
+		const children = item[childKey]
+
+		if (Array.isArray(children) && children.length) {
+			result.push(...untreeify(children, childKey))
+		}
+
+		delete item[childKey]
+	})
+
+	return result
+}
+
+export default { treeify, untreeify }
