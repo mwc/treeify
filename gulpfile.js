a6a5202246821bbe6be0ac51e751fc69961b88d2
MaoWenChao <mwc@foxmail.com>
2019-04-14 00:27:46 +0800

    modify build flow, add gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,36 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var rollup = require('gulp-rollup');
-var mocha = require('gulp-mocha');
+const gulp = require("gulp")
+const babel = require("gulp-babel")
+const rollup = require("gulp-rollup")
+const mocha = require("gulp-mocha")
+const uglify = require("gulp-uglify")
 
-gulp.task('rollup', function () {
-    return gulp.src('./src/treeify.js')
-        .pipe(rollup({
-            input: './src/treeify.js',
-            output: {
-                format: 'umd',
-                name: 'treeify'
-            }
-        }))
-        .pipe(gulp.dest('./build'));
-});
+gulp.task("rollup", function () {
+	return gulp.src("./src/treeify.js")
+		.pipe(
+			rollup({
+				input: "./src/treeify.js",
+				output: {
+					format: "umd",
+					name: "treeify"
+				}
+			})
+		)
+		.pipe(gulp.dest("./build"))
+})
 
-gulp.task('test', ['rollup'], function () {
-    return gulp.src('./tests/test.js')
-        .pipe(mocha());
-});
+gulp.task("test", ["rollup"], function () {
+	return gulp.src("./tests/*").pipe(mocha())
+})
 
-gulp.task('build', ['rollup'], function () {
-    return gulp.src('./build/treeify.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
-        .pipe(gulp.dest('./dist'));
-});
+gulp.task("build", ["rollup"], function () {
+	return gulp.src("./build/treeify.js")
+		.pipe(
+			babel({
+				presets: ["env"]
+			})
+		)
+		.pipe(uglify())
+		.pipe(gulp.dest("./dist"))
+})
 
-gulp.task('default', ['build']);
+gulp.task("default", ["build"])
